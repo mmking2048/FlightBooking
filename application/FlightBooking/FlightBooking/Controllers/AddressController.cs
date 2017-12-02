55aa451039369e01e93e62a030f0e31543fdc7b7
Michelle Yang <myang22@hawk.iit.edu>
2017-12-02 11:57:39 -0600

    Forgot create new for address

--- application/FlightBooking/FlightBooking/Controllers/AddressController.cs
+++ application/FlightBooking/FlightBooking/Controllers/AddressController.cs
@@ -18,6 +18,32 @@ namespace FlightBooking.Controllers
             return PartialView("Index", addresses);
         }
 
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "StreetNumber,StreetName,City,ZipCode,Country")] Address address)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    // TODO: database insert
+                    return RedirectToAction("Index", "Account");
+                }
+            }
+            catch (Exception /* dex */)
+            {
+                //Log the error (uncomment dex variable name and add a line here to write a log.)
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
+            }
+            
+            return View(address);
+        }
+
         public ActionResult Edit(int? id)
         {
             if (id == null)
