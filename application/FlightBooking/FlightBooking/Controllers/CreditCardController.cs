7162edd97c4e9309e66639d9c10dcccac717827a
Michelle Yang <myang22@hawk.iit.edu>
2017-12-02 11:49:07 -0600

    Finished views for address

---
+++ application/FlightBooking/FlightBooking/Controllers/CreditCardController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FlightBooking.Models;
+
+namespace FlightBooking.Controllers
+{
+    public class CreditCardController : Controller
+    {
+        [ChildActionOnly]
+        public ActionResult Index()
+        {
+            var creditCards = new[] {new CreditCard("Visa", "1111000011110000", "First", "Last", DateTime.Now, "111", 1)};
+            return PartialView("Index", creditCards);
+        }
+    }
+}
--- application/FlightBooking/FlightBooking/Models/Address.cs
+++ application/FlightBooking/FlightBooking/Models/Address.cs
@@ -1,10 +1,27 @@
-﻿namespace FlightBooking.Models
+﻿using System.ComponentModel;
+
+namespace FlightBooking.Models
 {
     public class Address
     {
+        public Address(int streetNumber, string streetName, string city, string state, string zipCode, string country, int addressID)
+        {
+            StreetNumber = streetNumber;
+            StreetName = streetName;
+            City = city;
+            State = state;
+            ZipCode = zipCode;
+            Country = country;
+            AddressID = addressID;
+        }
+
+        [DisplayName("Street Number")]
         public int StreetNumber { get; set; }
+        [DisplayName("Street Name")]
         public string StreetName { get; set; }
         public string City { get; set; }
+        public string State { get; set; }
+        [DisplayName("Zip Code")]
         public string ZipCode { get; set; }
         public string Country { get; set; }
         public long AddressID { get; set; }
