c08a82f3faa062453bf027ff40c41173d2478976
Michelle Yang <myang22@hawk.iit.edu>
2017-12-02 11:49:07 -0600

    Finished views for address

--- application/FlightBooking/FlightBooking/Models/CreditCard.cs
+++ application/FlightBooking/FlightBooking/Models/CreditCard.cs
@@ -1,13 +1,33 @@
 ﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace FlightBooking.Models
 {
     public class CreditCard
     {
+        public CreditCard(string type, string ccNumber, string cardFirstName, string cardLastName,
+            DateTime expirationDate, string cvc, int addressID)
+        {
+            Type = type;
+            CcNumber = ccNumber;
+            CardFirstName = cardFirstName;
+            CardLastName = cardLastName;
+            ExpirationDate = expirationDate;
+            Cvc = cvc;
+            AddressID = addressID;
+        }
+
+        [DisplayName("Provider")]
         public string Type { get; set; }
+        [DisplayName("Number")]
         public string CcNumber { get; set; }
+        [DisplayName("First Name")]
         public string CardFirstName { get; set; }
+        [DisplayName("Last Name")]
         public string CardLastName { get; set; }
+        [DisplayName("Expiration")]
+        [DisplayFormat(DataFormatString="{0:MM/yy}", ApplyFormatInEditMode = true)]
         public DateTime ExpirationDate { get; set; }
         public string Cvc { get; set; }
         public int AddressID { get; set; }
