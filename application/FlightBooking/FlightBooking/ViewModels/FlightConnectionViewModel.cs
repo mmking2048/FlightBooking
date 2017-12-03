99ec3962f99009e4c5101c9d1a11e131f0b65160
Michelle Yang <myang22@hawk.iit.edu>
2017-12-03 11:51:48 -0600

    Added view for flight search

---
+++ application/FlightBooking/FlightBooking/ViewModels/FlightConnectionViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FlightBooking.Models;
+
+namespace FlightBooking.ViewModels
+{
+    public class FlightConnectionViewModel
+    {
+        public FlightConnectionViewModel(Booking booking)
+        {
+            
+        }
+    }
+}
