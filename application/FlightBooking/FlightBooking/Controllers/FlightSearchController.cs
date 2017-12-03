a192b15e7823e5a05d78c134a7dcc11b615bfa9c
Michelle Yang <myang22@hawk.iit.edu>
2017-12-03 12:05:42 -0600

    Cleaned up usings

--- application/FlightBooking/FlightBooking/Controllers/FlightSearchController.cs
+++ application/FlightBooking/FlightBooking/Controllers/FlightSearchController.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices.ComTypes;
-using System.Web;
 using System.Web.Mvc;
 using FlightBooking.Models;
 using FlightBooking.ViewModels;
