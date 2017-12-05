1980b847d13c037b98f402ecbadd9e461b96f406
Michelle Yang <myang22@hawk.iit.edu>
2017-12-04 18:59:33 -0600

    Added view for cancelling

--- application/FlightBooking/FlightBooking/Models/Flight.cs
+++ application/FlightBooking/FlightBooking/Models/Flight.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace FlightBooking.Models
@@ -7,16 +8,22 @@ namespace FlightBooking.Models
     public class Flight
     {
         public DateTime Date { get; set; }
+        [DisplayName("Number")]
         public int FlightNumber { get; set; }
+        [DisplayName("Departure")]
         [DisplayFormat(DataFormatString = "{0:hh:mm tt}", ApplyFormatInEditMode = true)]
         public DateTimeOffset DepartureTime { get; set; }
+        [DisplayName("Arrival")]
         [DisplayFormat(DataFormatString = "{0:hh:mm tt}", ApplyFormatInEditMode = true)]
         public DateTimeOffset ArrivalTime { get; set; }
         public TimeSpan Length => ArrivalTime - DepartureTime;
+        [DisplayName("From")]
         public string DepartureAirport { get; set; }
+        [DisplayName("To")]
         public string ArrivalAirport { get; set; }
         public int MaxCoach { get; set; }
         public int MaxFirstClass { get; set; }
+        [DisplayName("Airline")]
         public string AirlineID { get; set; }
         public int BookedCoach { get; set; }
         public int BookedFirstClass { get; set; }
