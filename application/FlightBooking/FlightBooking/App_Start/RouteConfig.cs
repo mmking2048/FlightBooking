f33c0921c6d8b75fc94429852ee07ed32488c9c8
Michelle Yang <myang22@hawk.iit.edu>
2017-12-04 20:06:19 -0600

    Switched flight page to home page

--- application/FlightBooking/FlightBooking/App_Start/RouteConfig.cs
+++ application/FlightBooking/FlightBooking/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace FlightBooking
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "FlightSearch", action = "Search", id = UrlParameter.Optional }
             );
         }
     }
