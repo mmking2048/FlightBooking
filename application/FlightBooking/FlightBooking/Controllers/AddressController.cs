396f3afbfc799e001699d7094ab2edf27d7cf0fb
Michelle Yang <myang22@hawk.iit.edu>
2017-12-05 21:41:59 -0600

    Resolved problem with new address adding floozing

--- application/FlightBooking/FlightBooking/Controllers/AddressController.cs
+++ application/FlightBooking/FlightBooking/Controllers/AddressController.cs
@@ -37,7 +37,7 @@ namespace FlightBooking.Controllers
                     return RedirectToAction("Index", "Account");
                 }
             }
-            catch (Exception /* dex */)
+            catch (Exception e)
             {
                 //Log the error (uncomment dex variable name and add a line here to write a log.)
                 ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
