9de6ef21c93881a959359a2cf385f937d6272c61
Michelle Yang <myang22@hawk.iit.edu>
2017-12-09 14:31:05 -0600

    Cleaned up usings

--- application/FlightBooking/FlightBooking/Controllers/FlightSearchController.cs
+++ application/FlightBooking/FlightBooking/Controllers/FlightSearchController.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
-using FlightBooking.Models;
 using FlightBooking.ViewModels;
 
 namespace FlightBooking.Controllers
