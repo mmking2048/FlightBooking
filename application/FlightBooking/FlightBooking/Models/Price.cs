9de394e255691eb4fd3a0fcc23a2cd2d88a4fecd
jduggerIIT <jdugger@iit.edu>
2017-12-09 11:38:08 -0600

    Removed commented out attributes in Price model and ParsePrice

--- application/FlightBooking/FlightBooking/Models/Price.cs
+++ application/FlightBooking/FlightBooking/Models/Price.cs
@@ -6,19 +6,11 @@ namespace FlightBooking.Models
     {
         public string FlightClass { get; set; }
         public decimal Cost { get; set; }
-        /*
-        public DateTime Date { get; set; }
-        public int FlightNumber { get; set; }
-        public string Airline { get; set; }*/
 
-        public Price(string flightClass, decimal cost) //, DateTime date, int flightNumber, string airline)
+        public Price(string flightClass, decimal cost)
         {
             FlightClass = flightClass;
             Cost = cost;
-            /*
-            Date = date;
-            FlightNumber = flightNumber;
-            Airline = airline;*/
         }
     }
 }
